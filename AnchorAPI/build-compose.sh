02c662685ae981586cd80df6eabe7b8f40ad0f0e
Bogdan <bogdan.mastahac@gmail.com>
2020-04-15 14:06:38 +0300

    refactor paths

--- AnchorAPI/build-compose.sh
+++ AnchorAPI/build-compose.sh
@@ -1,4 +1,2 @@
 docker build -t ledger-anchorapi -f docker/Dockerfile .
-#docker run -it -p 3000:3000 --name=anchorapi --network=test ledger-anchorapi 
-
 docker-compose -f docker/docker-compose-anchorapi.yaml up -d

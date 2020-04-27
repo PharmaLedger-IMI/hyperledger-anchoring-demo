3c2109071622ed1b768e877bdd8dce54851ca1b6
Mastahac Bogdan <bogdan.mastahac@gmail.com>
2020-04-27 16:05:16 +0300

    clean up

--- ConfigurationContainer/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo Build image
-
-docker build -t ledger-emptydir -f dockerfile .
-docker tag ledger-emptydir:latest  mabdockerid/ledger-emptydir:latest
-
-echo push ledger image
-docker push mabdockerid/ledger-emptydir:latest

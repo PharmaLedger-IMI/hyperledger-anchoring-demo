f067a2a39896d0cd7df63311292203f24e7bc7b3
bmastahac <bogdan.mastahac@gmail.com>
2020-05-22 16:13:20 +0300

    added load balancer and post start event for peer pod

--- Kubernetes/helm/hlf-peer/clean.sh
+++ Kubernetes/helm/hlf-peer/clean.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-### Get orderer pod
-export ORD_POD=$(kubectl get pods --namespace pharma -l "app=hlf-ord" -o jsonpath="{.items[0].metadata.name}")
+
 
 
 ### Delete secrets
-kubectl -n pharma delete secret hlf--ord-admincert hlf--ord-cacert hlf--ord0-idcert hlf--ord0-idkey hlf--genesis
+kubectl -n pharma delete secret hlf--peer-admincert hlf--peer-adminkey hlf--peer-cacert hlf--peer0-idcert hlf--peer0-idkey hlf--channel
 
 ### Delete release
-#helm delete ORD_RELEASE_NAME -n pharma
+
 
 helm list -n pharma
 
